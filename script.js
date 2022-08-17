c9356712bda2197388b9d98a3f78c2653f0ce7b5
Charles <charlescyso@gmail.com>
2022-08-17 12:30:13 -0700

    Add style, test for mobile responsiveness

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ class UI {
         const div = document.createElement('div');
         div.className = `alert alert-${className}`;
         div.appendChild(document.createTextNode(message));
-        const container = document.querySelector('.container:nth-child(2)');
+        const container = document.querySelector('.book-form');
         const form = document.querySelector('#book-form');
         container.insertBefore(div, form);
 
